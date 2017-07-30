049fefbd675103be170111bd6cd694bbc85d1024
Pedro Gomes <pedro@loot.io>
2017-07-30 16:25:25 +0100

    add react-helmet

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Small Boutique Online Shop
 
-Example Online Shop project for Gabbana Girls using React/Redux
+Example Online Shop project using React/Redux
--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { Switch, Route } from 'react-router-dom';
+import Helmet from 'react-helmet';
 import FadeIn from './components/FadeIn';
 import Header from './components/Header';
 import Modal from './components/Modal';
@@ -9,7 +10,8 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Products from './pages/Products';
 import NotFound from './pages/NotFound';
-import { setSession, getSession } from './helpers/utilities';
+import Inventory from './libraries/inventory.json';
+import { setSession, getSession, titleCase } from './helpers/utilities';
 import './libraries/CSSTransitionAnimation.css';
 
 const CSSTransitionConfig = {
@@ -31,8 +33,11 @@ class Router extends Component {
   componentWillReceiveProps(nextProps, nextContext) {
     this.setState({ view: nextContext.router.route.location.pathname.slice(1) });
   }
+  getTitle = () =>
+    this.state.view ? `${titleCase(this.state.view)} - ${Inventory.storeName}` : Inventory.storeName;
   render = () => (
     <FadeIn>
+      <Helmet title={this.getTitle()} />
       <Header view={this.state.view} />
       <Switch>
         <Route
